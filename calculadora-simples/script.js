ff99b39e1b733202ead66c27c37505e884540309
Daniel-Alves <d4ni3llllllll@gmail.com>
2025-02-10 10:46:49 -0300

    Update script.js

--- calculadora-simples/script.js
+++ calculadora-simples/script.js
@@ -11,7 +11,7 @@
     function calculateResult() {
       const display = document.getElementById('display');
       try {
-        // Usa Function para avaliar a expressão de forma mais segura
+       
         display.value = new Function('return ' + display.value)();
       } catch (error) {
         alert('Entrada Inválida');
